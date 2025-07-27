edcf896d09d002a1fd45f99928a1e8b2a58e2019
Derek Vincent <derek.vincent@kemikal.net>
2025-07-26 22:54:27 -0400

    Refactor entrypoint.sh to remove awesome-lists-generator installation check and update generate command; add GitHub Action for tagging and releasing awesome lists.

--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -20,11 +20,5 @@ if [ -n "$GITHUB_WORKSPACE" ]; then
     cd "$GITHUB_WORKSPACE"
 fi
 
-# Check if the awesome-lists-generator is installed
-#if ! command -v awesome-lists-generator &> /dev/null; then
-#    echo "awesome-lists-generator could not be found. Please ensure it is installed."
-#    exit 1
-#fi
-
 set -x
-awesome-lists-generator generate $INPUT_LISTS_FILE 
+awesome-lists-generator generate $INPUT_LISTS_FILE $GENERATE_ARGS
